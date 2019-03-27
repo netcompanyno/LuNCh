8d7b26fa71e442f8b8499a4b9a8b2bcbb65e244e
Lars Dahl <larsd@netcompany.com>
2019-03-27 20:19:00 +0100

    Add mutation and service method for registering events

--- src/main/java/no/netcompany/apps/lunch/resolver/Mutation.kt
+++ src/main/java/no/netcompany/apps/lunch/resolver/Mutation.kt
@@ -2,11 +2,13 @@ package no.netcompany.apps.lunch.resolver
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver
 import no.netcompany.apps.lunch.model.LunchEvent
+import no.netcompany.apps.lunch.service.RegistrationService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class Mutation: GraphQLMutationResolver {
-    fun registerEvent(event: LunchEvent?): LunchEvent? {
-        return null
+class Mutation(@Autowired private val registrationService: RegistrationService): GraphQLMutationResolver {
+    fun registerEvent(uid: String): LunchEvent? {
+        return registrationService.createEvent(uid)
     }
 }
--- src/main/java/no/netcompany/apps/lunch/service/RegistrationService.kt
+++ src/main/java/no/netcompany/apps/lunch/service/RegistrationService.kt
@@ -6,13 +6,23 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import no.netcompany.apps.lunch.model.LunchEvent
 import no.netcompany.apps.lunch.model.Person
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.io.IOException
+import java.util.*
 
 @Component
 class RegistrationService(@Autowired private val firebaseDatabase: FirebaseDatabase) {
+    fun createEvent(uid: String): LunchEvent? {
+        val eventsRef = firebaseDatabase.getReference("events")
+        val createdDate = Date().toInstant()
+        val newEvent = LunchEvent(uid = uid, created = createdDate.toString())
+        eventsRef.push().setValueAsync(newEvent)
+
+        return newEvent
+    }
 
     fun getPersons(): ApiFuture<List<Person>> {
         val personsFuture = SettableApiFuture.create<List<Person>>()
